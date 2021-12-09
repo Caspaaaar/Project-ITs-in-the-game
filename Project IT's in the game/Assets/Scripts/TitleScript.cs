d2db7c0beb7e2fd81fea967b5ca4c12429748a24
RubendeGroot <rubendegroot2509@gmail.com>
2021-12-09 12:51:14 +0100

    Title screen added

---
+++ Project IT's in the game/Assets/Scripts/TitleScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    //Press any key to continue
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        }
+    }
+}
