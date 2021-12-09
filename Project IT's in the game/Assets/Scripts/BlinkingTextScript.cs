a671241a9f7ba547b55c8a58c4ad253217a60fae
RubendeGroot <rubendegroot2509@gmail.com>
2021-12-09 14:12:34 +0100

    blinking text script

---
+++ Project IT's in the game/Assets/Scripts/BlinkingTextScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingTextScript : MonoBehaviour
+{
+
+    public MaskableGraphic imageToToggle;
+
+    public float interval = 1f;
+    public float startDelay = 0.5f;
+    public bool currentState = true;
+    public bool defaultState = true;
+    bool isBlinking = false;
+
+    public AudioClip clip;
+
+    void Start()
+    {
+        imageToToggle.enabled = defaultState;
+        StartBlink();
+    }
+
+    public void StartBlink()
+    {
+        // do not invoke the blink twice - needed if you need to start the blink from an external object
+        if (isBlinking)
+            return;
+
+        if (imageToToggle != null)
+        {
+            isBlinking = true;
+            InvokeRepeating("ToggleState", startDelay, interval);
+        }
+    }
+
+    public void ToggleState()
+    {
+        imageToToggle.enabled = !imageToToggle.enabled;
+    }
+
+}
